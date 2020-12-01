21230b6e5f478f6059363b1759d79a0b14e8cee6
BessonMikael <besson.mikael04@gmail.com>
2020-12-01 12:00:59 +0100

    ajout de la condition "&&"

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 let a =document.getElementById("monDiv");
 console.log(a);
 for(let i = 0; i <51; i++){
-    if (i<18){
+    if (i<19){
         a.innerHTML += i + " ans " + "le capitaine est jeune" + "<br><br>";
     }
-    if (i>=18){
+    else if (i>18 && i<50){
         a.innerHTML += i+ " ans " + "le capitaine n'est pas tres vieux" + "<br><br>";
     }
-    if (i>49){
+    else if (i>49){
         a.innerHTML += i+ " ans " + "le capitaine est vieux" + "<br><br>";
     }
 }
